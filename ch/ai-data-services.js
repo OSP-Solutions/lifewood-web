1472e25db547abf7423c112db6ed1b2c3fcb7633
ignjat91 <predrag.ignjatovic@netcast.rs>
2024-04-24 00:52:47 +0200

    ch start rewrite

---
+++ ch/ai-data-services.js
@@ -0,0 +1,126 @@
+
+      function goToYoutubeChannel() {
+        window.location.href = "https://youtube.com/@LifewoodDataTechnology/videos";
+      }
+
+      function goToYoutubeVideo() {
+        window.location.href = "https://www.youtube.com/watch?v=9LjMAJrOirU"
+      }
+      var lifewood = document.getElementById("lifewood");
+      if (lifewood) {
+        lifewood.addEventListener("click", function (e) {
+          window.location.href = "home-page1.html";
+        });
+      }
+
+      var button = document.getElementById("button");
+      if (button) {
+        button.addEventListener("click", function (e) {
+          window.location.href = "./ai-projects.html";
+        });
+      }
+
+      var buttonContainer1 = document.getElementById("buttonContainer1");
+      if (buttonContainer1) {
+        buttonContainer1.addEventListener("click", function (e) {
+          window.location.href = "./tansformation.html";
+        });
+      }
+
+      var buttonContainer2 = document.getElementById("buttonContainer2");
+      if (buttonContainer2) {
+        buttonContainer2.addEventListener("click", function (e) {
+          window.location.href = "./global-scale.html";
+        });
+      }
+
+      var buttonContainer3 = document.getElementById("buttonContainer3");
+      if (buttonContainer3) {
+        buttonContainer3.addEventListener("click", function (e) {
+          window.location.href = "./our-company.html";
+        });
+      }
+
+      var buttonContainer4 = document.getElementById("buttonContainer4");
+      if (buttonContainer4) {
+        buttonContainer4.addEventListener("click", function (e) {
+          window.location.href = "./careers.html";
+        });
+      }
+
+      var button1 = document.getElementById("button1");
+      if (button1) {
+        button1.addEventListener("click", function (e) {
+          window.location.href = "./contact.html";
+        });
+      }
+
+      var logo = document.getElementById("logo");
+      if (logo) {
+        logo.addEventListener("click", function (e) {
+          window.location.href = "home-page1.html";
+        });
+      }
+
+      var buttonContainer = document.getElementById("buttonContainer");
+      if (buttonContainer) {
+        buttonContainer.addEventListener("click", function (e) {
+          window.location.href = "./contact.html";
+        });
+      }
+
+      var aIDataServices = document.getElementById("aIDataServices");
+      if (aIDataServices) {
+        aIDataServices.addEventListener("click", function (e) {
+          window.location.href = "home-page1.html";
+        });
+      }
+
+      var aIProjectsText = document.getElementById("aIProjectsText");
+      if (aIProjectsText) {
+        aIProjectsText.addEventListener("click", function (e) {
+          window.location.href = "./ai-projects.html";
+        });
+      }
+
+      var transformationText = document.getElementById("transformationText");
+      if (transformationText) {
+        transformationText.addEventListener("click", function (e) {
+          window.location.href = "./tansformation.html";
+        });
+      }
+
+      var globalScaleText = document.getElementById("globalScaleText");
+      if (globalScaleText) {
+        globalScaleText.addEventListener("click", function (e) {
+          window.location.href = "./global-scale.html";
+        });
+      }
+
+      var ourCompanyText = document.getElementById("ourCompanyText");
+      if (ourCompanyText) {
+        ourCompanyText.addEventListener("click", function (e) {
+          window.location.href = "./our-company.html";
+        });
+      }
+
+      var careersText = document.getElementById("careersText");
+      if (careersText) {
+        careersText.addEventListener("click", function (e) {
+          window.location.href = "./careers.html";
+        });
+      }
+
+      var englishText = document.getElementById("englishText");
+      if (englishText) {
+        englishText.addEventListener("click", function (e) {
+          window.location.href = "/index.html";
+        });
+      }
+
+      var chineseText = document.getElementById("chineseText");
+      if (chineseText) {
+        chineseText.addEventListener("click", function (e) {
+          window.location.href = "/ch/index.html";
+        });
+      }
---
+++ ch/index.js
@@ -0,0 +1,178 @@
+
+  function goToYoutubeChannel() {
+    window.location.href = "https://youtube.com/@LifewoodDataTechnology/videos";
+  }
+  var lifewood = document.getElementById("lifewood");
+  if (lifewood) {
+    lifewood.addEventListener("click", function (e) {
+      window.location.href = "home-page1.html";
+    });
+  }
+
+  var buttonContainer = document.getElementById("buttonContainer0");
+  if (buttonContainer) {
+    buttonContainer.addEventListener("click", function (e) {
+      window.location.href = "./ai-data-services.html";
+    });
+  }
+
+  var buttonContainer1 = document.getElementById("buttonContainer1");
+  if (buttonContainer1) {
+    buttonContainer1.addEventListener("click", function (e) {
+      window.location.href = "./ai-projects.html";
+    });
+  }
+
+  var buttonContainer2 = document.getElementById("buttonContainer2");
+  if (buttonContainer2) {
+    buttonContainer2.addEventListener("click", function (e) {
+      window.location.href = "./tansformation.html";
+    });
+  }
+
+  var buttonContainer3 = document.getElementById("buttonContainer3");
+  if (buttonContainer3) {
+    buttonContainer3.addEventListener("click", function (e) {
+      window.location.href = "./global-scale.html";
+    });
+  }
+
+  var buttonContainer4 = document.getElementById("buttonContainer4");
+  if (buttonContainer4) {
+    buttonContainer4.addEventListener("click", function (e) {
+      window.location.href = "./our-company.html";
+    });
+  }
+
+  var buttonContainer5 = document.getElementById("buttonContainer5");
+  if (buttonContainer5) {
+    buttonContainer5.addEventListener("click", function (e) {
+      window.location.href = "./careers.html";
+    });
+  }
+
+  var buttonContainer6 = document.getElementById("readMoreProjects");
+  if (buttonContainer6) {
+    buttonContainer6.addEventListener("click", function (e) {
+      window.location.href = "./ai-projects.html";
+    });
+  }
+
+  var button = document.getElementById("button");
+  if (button) {
+    button.addEventListener("click", function (e) {
+      window.location.href = "./contact.html";
+    });
+  }
+
+  var button1 = document.getElementById("button1");
+  if (button1) {
+    button1.addEventListener("click", function (e) {
+      window.location.href = "./global-scale.html";
+    });
+  }
+
+  var buttonContainer = document.getElementById("buttonContainer");
+  if (buttonContainer) {
+    buttonContainer.addEventListener("click", function (e) {
+      window.location.href = "./contact.html";
+    });
+  }
+
+  var buttonContainer5 = document.getElementById(
+    "buttonContainerGenealogy"
+  );
+  if (buttonContainer5) {
+    buttonContainer5.addEventListener("click", function (e) {
+      window.location.href = "./genealogy.html";
+    });
+  }
+
+  var singleItemContainer5 = document.getElementById(
+    "singleItemContainer5"
+  );
+  if (singleItemContainer5) {
+    singleItemContainer5.addEventListener("click", function (e) {
+      window.location.href = "./ai-data-services.html";
+    });
+  }
+
+  var buttonContainer = document.getElementById("buttonContainer");
+  if (buttonContainer) {
+    buttonContainer.addEventListener("click", function (e) {
+      window.location.href = "./contact.html";
+    });
+  }
+
+  var logo = document.getElementById("logo");
+  if (logo) {
+    logo.addEventListener("click", function (e) {
+      window.location.href = "home-page1.html";
+    });
+  }
+
+  var aIDataServices = document.getElementById("aIDataServices");
+  if (aIDataServices) {
+    aIDataServices.addEventListener("click", function (e) {
+      window.location.href = "home-page1.html";
+    });
+  }
+
+  var aIProjectsText = document.getElementById("aIProjectsText");
+  if (aIProjectsText) {
+    aIProjectsText.addEventListener("click", function (e) {
+      window.location.href = "./ai-projects.html";
+    });
+  }
+
+  var transformationText = document.getElementById("transformationText");
+  if (transformationText) {
+    transformationText.addEventListener("click", function (e) {
+      window.location.href = "./tansformation.html";
+    });
+  }
+
+  var globalScaleText = document.getElementById("globalScaleText");
+  if (globalScaleText) {
+    globalScaleText.addEventListener("click", function (e) {
+      window.location.href = "./global-scale.html";
+    });
+  }
+
+  var ourCompanyText = document.getElementById("ourCompanyText");
+  if (ourCompanyText) {
+    ourCompanyText.addEventListener("click", function (e) {
+      window.location.href = "./our-company.html";
+    });
+  }
+
+  var careersText = document.getElementById("careersText");
+  if (careersText) {
+    careersText.addEventListener("click", function (e) {
+      window.location.href = "./careers.html";
+    });
+  }
+
+  var lanCh = document.getElementById("buttonCh");
+  if (lanCh) {
+    lanCh.addEventListener("click", function (e) {
+      window.location.href = "../";
+    });
+  }
+
+  var englishText = document.getElementById("englishText");
+  if (englishText) {
+    englishText.addEventListener("click", function (e) {
+      window.location.href = "/index.html";
+    });
+  }
+
+  var chineseText = document.getElementById("chineseText");
+  if (chineseText) {
+    chineseText.addEventListener("click", function (e) {
+      window.location.href = "/ch/index.html";
+    });
+  }
+
+
+
